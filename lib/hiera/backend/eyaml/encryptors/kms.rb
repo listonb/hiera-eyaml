7e7dc70466134dc56fcbb0cad3f1a9e83029ab72
Bryan Liston <bryanl@slalom.com>
2015-12-17 11:58:14 -0800

    Working version of KMS for eyaml

--- lib/hiera/backend/eyaml/encryptors/kms.rb
+++ lib/hiera/backend/eyaml/encryptors/kms.rb
@@ -1,6 +1,9 @@
 require 'hiera/backend/eyaml/encryptor'
 require 'hiera/backend/eyaml/utils'
+require 'hiera/backend/eyaml/plugins'
 require 'hiera/backend/eyaml/options'
+require 'aws-sdk'
+require 'base64'
 
 class Hiera
   module Backend
@@ -8,17 +11,33 @@ class Hiera
       module Encryptors
 
         class Kms < Encryptor
-
+          
+          self.options = {
+            :region => { :desc => "AWS Region", 
+                              :type => :string, 
+                              :default => "us-east-1" },
+            :key => { :desc => "KMS KeyID",
+                      :type => :string }
+          }
+          
+          region = self.option :region
+          
+          
           self.tag = "KMS"
 
           def self.encrypt plaintext
+            region = self.option :region
+            key = self.option :key
+            raise StandardError, "KMS KeyId Required" unless key
             
+            kms = Aws::KMS::Client.new(region: region)
+            kms.encrypt(key_id: key, plaintext: plaintext).ciphertext_blob
           end
 
           def self.decrypt ciphertext
-            
-            raise StandardError, "decrypt"
-
+            region = self.option :region            
+            kms = Aws::KMS::Client.new(region: region)
+            kms.decrypt(ciphertext_blob: Base64.decode64(Base64.encode64(ciphertext))).plaintext
           end
 
         end
